819ba5c8c98644e13479a052be6fa7ddbb35b9c3
shapovalovaaa <nastyashpovalova70@gmail.com>
2023-08-02 19:14:48 +0300

    feat: solve task

--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -2,6 +2,17 @@ package school.mjc.stage0.module3.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        //write code here
+        int first = 1;
+        int second = 10;
+        int third = 100;
+        System.out.println(first + "\n" + second + "\n" + third);
+
+        int linkToFirst = first;
+        int linkToSecond = second;
+        int linkToThird = third;
+        linkToFirst = 15;
+        linkToSecond = 6;
+        linkToThird = 4;
+        System.out.println(linkToFirst + "\n" + linkToSecond + "\n" + linkToThird + "\n" + first + "\n" + second + "\n" + third);
     }
 }
